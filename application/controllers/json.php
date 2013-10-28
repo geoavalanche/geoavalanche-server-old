59e84b872c434cad3e2bedeac5be31c3083892f5
francesco bartoli <francesco.bartoli@geobeyond.it>
2013-10-28 19:08:21 -0400

    review of json controller after upgrade to 2.7.1

--- application/controllers/json.php
+++ application/controllers/json.php
@@ -803,11 +803,11 @@ class Json_Controller extends Template_Controller {
 		foreach ($cluster as $marker)
 		{
 			// Normalising data
-			if (is_array($marker))
-			{
-				$marker = (object) $marker;
-			}
-
+ 		        if (is_array($marker))
+		        {
+		          $marker = (object) $marker;
+		        }
+ 
 			// Handle both reports::fetch_incidents() response and actual ORM objects
 			$latitude = isset($marker["latitude"]) ? $marker["latitude"] : $marker["location"]["latitude"];
 			$longitude = isset($marker["longitude"]) ? $marker["longitude"] : $marker["location"]["longitude"];
